7581b35c1c6aec20cb69c532a12c163f6974369b
soul-cc <saulc10c@gmail.com>
2023-08-31 22:34:44 -0500

    commit para el proyecto de React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
 }
+
+function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [formData2, setFormData2] = useReducer(formReducer, {});
+  const [formData3, setFormData3] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 5000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange2 = event => {
+    setFormData2({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange3 = event => {
+    setFormData3({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+
+  return(
+    <div className="estilo">
+      <h1>Usando formularios y eventos en React</h1>
+      {submitting &&
+       <div>
+         Tu realizaste Submit para la siguiente información:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>Nombres</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData2).map(([name, value]) => (
+             <li key={name}><strong>Apellidos</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData3).map(([name, value]) => (
+             <li key={name}><strong>Universidad</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
+      <form className="estiloform" onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Nombres</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Apellidos</p>
+            <input name="ape" onChange={handleChange2}/>
+          </label>
+          <label>
+            <p>Universidad</p>
+            <input name="ape" onChange={handleChange3}/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
 
 export default App;
